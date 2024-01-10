3fd06563e61e9cba0263dc70e23a9ad2741e0bc5
chorcharg <jorgevmanci@gmail.com>
2024-01-09 23:27:11 -0300

    Client + Notes (endpoints separados)

---
+++ src/main/java/com/lito/taller/service/noteSupport/NoteType.java
@@ -0,0 +1,9 @@
+package com.lito.taller.service.noteSupport;
+
+import com.lito.taller.entity.Note;
+
+public interface NoteType {
+
+    Note createNote(long id);
+
+}
---
+++ src/main/java/com/lito/taller/service/noteSupport/impl/ClientNote.java
@@ -0,0 +1,25 @@
+package com.lito.taller.service.noteSupport.impl;
+
+import com.lito.taller.dto.Client.ClientDTO;
+import com.lito.taller.entity.Note;
+import com.lito.taller.service.ClientService;
+import com.lito.taller.service.noteSupport.NoteType;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+public class ClientNote implements NoteType {
+
+    @Autowired
+    ClientService clientService;
+
+    @Override
+    public Note createNote(long id) {
+        ClientDTO clientDTO = clientService.getById( id );
+
+        Note note  = new Note();
+        note.setClient(clientDTO.mapToEntity());
+
+        return note;
+    }
+}
