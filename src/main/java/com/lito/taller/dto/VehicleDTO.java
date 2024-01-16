f51f32bccaafcad16a8389fdad3fa2b50945258b
Matias Figueroa <matiasarielfigueroa@gmail.com>
2024-01-15 23:38:38 -0300

    Se agrega VehicleDTO

--- src/main/java/com/lito/taller/dto/VehicleDTO.java
+++ src/main/java/com/lito/taller/dto/VehicleDTO.java
@@ -1,4 +1,29 @@
 package com.lito.taller.dto;
 
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VehicleDTO {
+
+    long id;
+
+    String car_number;
+
+    String brand;
+
+    String model;
+
+    int year;
+
+    String summary;
+
 }
